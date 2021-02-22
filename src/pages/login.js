6bd6702121174f271e9861fb0c334c6a629c5fe7
Kael Henrique da Silva <kael_hs@hotmail.com>
2021-02-22 15:10:35 -0300

    sidebar css adjust 6

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginForm from '../components/LoginForm/LoginForm';
+import LoginForm from '../components/LoginForm/LoginForm.js';
 
 const Login = () => {
     return (
