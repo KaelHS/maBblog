cad6fb189bc930a69411f2e6fe1634e3c4518ad0
Kael Henrique da Silva <kael_hs@hotmail.com>
2021-02-24 17:31:14 -0300

    Initial about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,65 @@
 import React from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
 
+
+const Container = styled.body`
+    box-size: border-box;
+    position: fixed;
+    width: 100%;
+    height:100%;
+    background: #3e2093;
+    margin:0px;
+    padding:0px;
+
+    &::before, &::after {
+        display: table;
+        content:'';
+        clear: both;
+    }
+`;
+
+const TextContainer = styled.div`
+    margin: 100px 0 auto 30px;
+    float: left;
+    max-width: 400px;
+    padding: 10px;
+
+`;
+const Title = styled.h1`
+    color: #fff;
+    font-size: 3em;
+    margin-bottom: 20px;
+`;
+
+const Paragrafo = styled.p`
+    color: #fff;
+    margin-bottom: 10px;
+`;
+
+const ImgContainer = styled.div`
+    margin: 50px 0 auto 150px;
+    float: left;
+    max-width: 600px;
+    padding: 10px;
+
+    img {
+        display: block;
+        filter: blur(2px);
+    }
+`;
 const About = () => {
     return (
-        <div>
-            <h1 style={{fontFamily: "zilap-africa"}}>EU SOU POR QUE NÓS SOMOS</h1>
-            <Link href="/"><a><button type="button">Home</button></a></Link>
-        </div>
+        <Container>
+            <TextContainer>
+            <Title> Sobre mim...</Title>
+            <Paragrafo>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</Paragrafo>
+            <Link href="/"><a><button type="button">Home</button></a></Link>    
+            </TextContainer>
+            <ImgContainer>
+                <img src="/ubuntu-me.png" />
+            </ImgContainer>
+        </Container>
     )
 }
 
