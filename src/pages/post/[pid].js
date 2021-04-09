590254c6a5ee32e306b8bdef2b18ddb15f3902f0
Kael Henrique da Silva <kael_hs@hotmail.com>
2021-04-09 01:33:11 -0300

    Fixing to deploy2

--- src/pages/post/[pid].js
+++ src/pages/post/[pid].js
@@ -1,26 +1,16 @@
-<<<<<<< HEAD
 import React from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router'
 
-const router = useRouter()
-const { pid } = router.query
 
-const Post = () => {
-
-=======
-import { useRouter } from 'next/router'
 
 const Post = () => {
   const router = useRouter()
   const { pid } = router.query
->>>>>>> cad6fb189bc930a69411f2e6fe1634e3c4518ad0
 
   return <p>Post: {pid}</p>
 }
 
-<<<<<<< HEAD
-export default Post;
 
 export const getStaticProps = async ( context ) => {
 
@@ -41,6 +31,5 @@ export async function getStaticPaths () {
     fallback: false,
   }
 }
-=======
-export default Post
->>>>>>> cad6fb189bc930a69411f2e6fe1634e3c4518ad0
+
+export default Post;
