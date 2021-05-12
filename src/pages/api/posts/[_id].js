f949608054f1362650909bd3c9faa1282a2d2185
Kael Henrique da Silva <kael_hs@hotmail.com>
2021-05-12 04:05:36 -0300

    Tech Images List

--- src/pages/api/posts/[_id].js
+++ src/pages/api/posts/[_id].js
@@ -1,23 +1,23 @@
-const connect = require ('../../../config/database');
+// const connect = require ('../../../config/database');
 
-export default async (req, res) => {
+// export default async (req, res) => {
 
-    if (req.method === 'GET') {
+//     if (req.method === 'GET') {
 
-       const { title } = req.query;
+//        const { title } = req.query;
            
-       const { db } = await connect();
+//        const { db } = await connect();
        
-       const response =  await db.collection('posts').findOne({ title: title });
+//        const response =  await db.collection('posts').findOne({ title: title });
 
-       if(!response) {
-           res.status(400).json({ error: "Title not Found! " })
-       }
+//        if(!response) {
+//            res.status(400).json({ error: "Title not Found! " })
+//        }
 
-       res.status(200).json(response);
+//        res.status(200).json(response);
 
-   } else {
-           res.setHeader('Allow', ['GET'])
-           res.status(405).end(`Method ${method} Not Allowed`)
-   }
-}
+//    } else {
+//            res.setHeader('Allow', ['GET'])
+//            res.status(405).end(`Method ${method} Not Allowed`)
+//    }
+// }
