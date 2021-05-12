96f218cd677789455590b69dff9ee21763a01c1e
Kael Henrique da Silva <kael_hs@hotmail.com>
2021-05-12 04:12:14 -0300

    Tech Images List

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,123 +1,123 @@
-import React from 'react';
-import Sidebar from '../components/SideBar';
-import styled from 'styled-components';
-import Link from 'next/link';
-import axios from 'axios';
+// import React from 'react';
+// import Sidebar from '../components/SideBar';
+// import styled from 'styled-components';
+// import Link from 'next/link';
+// import axios from 'axios';
 
 
-const Container = styled.div`
+// const Container = styled.div`
 
-    float: right;
-    height: 100%;
-    right: 0;
-    position: fixed;
-    width: 80%;
-    background: #0e0b16;
-    border-left: 2px solid #fff;
-    overflow-y: scroll;
+//     float: right;
+//     height: 100%;
+//     right: 0;
+//     position: fixed;
+//     width: 80%;
+//     background: #0e0b16;
+//     border-left: 2px solid #fff;
+//     overflow-y: scroll;
 
-`;
+// `;
 
-const Postcard = styled.div`
-    border-bottom: 2px solid #fff;
+// const Postcard = styled.div`
+//     border-bottom: 2px solid #fff;
 
-    &::before {
-        content: '';
-        display: table;
-        clear: both;
-    }
-    &::after {
-        content: '';
-        display: table;
-        clear: both;
-    }
-    &:hover {
-        cursor: pointer;
-        background: #363636;
-        transition: 0.2s;
-    }
-`;
+//     &::before {
+//         content: '';
+//         display: table;
+//         clear: both;
+//     }
+//     &::after {
+//         content: '';
+//         display: table;
+//         clear: both;
+//     }
+//     &:hover {
+//         cursor: pointer;
+//         background: #363636;
+//         transition: 0.2s;
+//     }
+// `;
 
-const ImgCard = styled.div `
-    width: 100px;
-    height: 100px;
-    background: #e54;
-    margin:40px;
-    border-radius: 50%;
-    display: block;
-    float: left;
-`;
+// const ImgCard = styled.div `
+//     width: 100px;
+//     height: 100px;
+//     background: #e54;
+//     margin:40px;
+//     border-radius: 50%;
+//     display: block;
+//     float: left;
+// `;
 
-const TextCard = styled.div`
-    float: left;
-    margin-left: 30px;
-    color: #fff;
-`;
-const Data = styled.h5`
-    margin-top: 20px;
-    margin-bottom: 10px;
-`;
+// const TextCard = styled.div`
+//     float: left;
+//     margin-left: 30px;
+//     color: #fff;
+// `;
+// const Data = styled.h5`
+//     margin-top: 20px;
+//     margin-bottom: 10px;
+// `;
 
-const Titulo = styled.h2`
-        font-family: monospace;
-        text-transform: uppercase;
-        font-size: 2em;
-        font-weight: bold;
-        margin-bottom: 30px;
+// const Titulo = styled.h2`
+//         font-family: monospace;
+//         text-transform: uppercase;
+//         font-size: 2em;
+//         font-weight: bold;
+//         margin-bottom: 30px;
 
-`;
+// `;
 
-const Texto = styled.p`
-    font-size: 1.2em;
-    font-family: Helvetica, Arial;
-`;
+// const Texto = styled.p`
+//     font-size: 1.2em;
+//     font-family: Helvetica, Arial;
+// `;
 
 
 
-export const getStaticProps = async () => {
+// // export const getStaticProps = async () => {
 
-    const {data} = await axios.get('http://localhost:3000/api/posts');
+// //     const {data} = await axios.get('http://localhost:3000/api/posts');
   
   
-    return {
-        props: {
-            posts: data,
-        }
-    }
+// //     return {
+// //         props: {
+// //             posts: data,
+// //         }
+// //     }
   
-  }
+// //   }
 
-export default function Blog( {posts} ) {
+// export default function Blog( {posts} ) {
 
-    const [postList, setPostList] = React.useState([]);
+//     const [postList, setPostList] = React.useState([]);
 
-    setPostList(posts);
+//     setPostList(posts);
 
-     function PostCard ( title, date, content )  {
+//      function PostCard ( title, date, content )  {
 
-         return(
-                <Link href="#"><a>
-                <Postcard >
-                <ImgCard />
-                <TextCard>
-                    <Data>{date}</Data>
-                    <Titulo>{title}</Titulo>
-                    <Texto>{content}</Texto>
-                </TextCard> 
-            </Postcard>
-            </a></Link>
-        );
-    }
+//          return(
+//                 <Link href="#"><a>
+//                 <Postcard >
+//                 <ImgCard />
+//                 <TextCard>
+//                     <Data>{date}</Data>
+//                     <Titulo>{title}</Titulo>
+//                     <Texto>{content}</Texto>
+//                 </TextCard> 
+//             </Postcard>
+//             </a></Link>
+//         );
+//     }
 
-    return(
-        <React.Fragment>
-        <Sidebar />
-        <Container>
-        {
-            postList.map( post => {
-            return <PostCard key={post._id} posts={post}/>})} 
-        </Container>
-        </React.Fragment>
+//     return(
+//         <React.Fragment>
+//         <Sidebar />
+//         <Container>
+//         {
+//             postList.map( post => {
+//             return <PostCard key={post._id} posts={post}/>})} 
+//         </Container>
+//         </React.Fragment>
         
-    );
-}
+//     );
+// }
--- src/pages/gestor/create.js
+++ src/pages/gestor/create.js
@@ -1,136 +1,136 @@
-import React from 'react'
-import axios from 'axios';
-import styled from 'styled-components';
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
-import { useRouter } from 'next/router';
+// import React from 'react'
+// import axios from 'axios';
+// import styled from 'styled-components';
+// import Swal from 'sweetalert2';
+// import withReactContent from 'sweetalert2-react-content';
+// import { useRouter } from 'next/router';
  
-const FormPageContainer = styled.div`
-    margin: auto;
-    padding: 10px;
-    max-width: 600px;
-    background: #fff;
-    border-radius: 14px;
-    align-items: center;
+// const FormPageContainer = styled.div`
+//     margin: auto;
+//     padding: 10px;
+//     max-width: 600px;
+//     background: #fff;
+//     border-radius: 14px;
+//     align-items: center;
 
-`;  
+// `;  
 
-const FormPost = styled.form`
-    margin: 20px;
-`;
+// const FormPost = styled.form`
+//     margin: 20px;
+// `;
 
-const Label = styled.label`
-    width: 100%;
-    font-size: 1em;
-    line-height: 2em;
-    margin: 2px;
-`;
+// const Label = styled.label`
+//     width: 100%;
+//     font-size: 1em;
+//     line-height: 2em;
+//     margin: 2px;
+// `;
   
-const Input = styled.input`
-    width: 100%;
-    font-size: 1.2em;
-    margin-top: 5px;
-    margin-bottom: 10px;
-    padding: 5px;
-    border: 2px solid#00acee;
-    border-radius: 7px;
-`;  
+// const Input = styled.input`
+//     width: 100%;
+//     font-size: 1.2em;
+//     margin-top: 5px;
+//     margin-bottom: 10px;
+//     padding: 5px;
+//     border: 2px solid#00acee;
+//     border-radius: 7px;
+// `;  
 
-const TextArea = styled.textarea.attrs(props => ({
-    type: "text",
-}))`
-    width: 100%;
-    font-size: 1.2em;
-    margin-top: 10px;
-    margin-bottom: 10px;
-    border: 2px solid #00acee;
-    border-radius: 7px;
-    height: 18rem;
-    background: #fffafa;
-`;
+// const TextArea = styled.textarea.attrs(props => ({
+//     type: "text",
+// }))`
+//     width: 100%;
+//     font-size: 1.2em;
+//     margin-top: 10px;
+//     margin-bottom: 10px;
+//     border: 2px solid #00acee;
+//     border-radius: 7px;
+//     height: 18rem;
+//     background: #fffafa;
+// `;
 
-const SubmitInput = styled.input.attrs(props => ({
-    type: "submit",
-}))`
-    width: 300px;
-    height: 40px;
-    border-radius: 4px;
-    border: 1px solid #00acee;
-    margin: auto;
+// const SubmitInput = styled.input.attrs(props => ({
+//     type: "submit",
+// }))`
+//     width: 300px;
+//     height: 40px;
+//     border-radius: 4px;
+//     border: 1px solid #00acee;
+//     margin: auto;
 
-    &:hover {
-    font-weight: bold;
-    background: #00acee;
-    color: #fff;
-    transition: 0.5s;
-    cursor: pointer;
-    }
-`;
+//     &:hover {
+//     font-weight: bold;
+//     background: #00acee;
+//     color: #fff;
+//     transition: 0.5s;
+//     cursor: pointer;
+//     }
+// `;
 
 
-const Create = () => {
+// const Create = () => {
 
-    const router = useRouter();
-    const customAlert = withReactContent(Swal);
+//     const router = useRouter();
+//     const customAlert = withReactContent(Swal);
 
-    const [ title, setTitle ] = React.useState('');
-    const [ author, setAuthor ] = React.useState('');
-    const [ content, setContent ] = React.useState('');
+//     const [ title, setTitle ] = React.useState('');
+//     const [ author, setAuthor ] = React.useState('');
+//     const [ content, setContent ] = React.useState('');
 
-    function handleSubmit (event) {
-        event.preventDefault();
+//     function handleSubmit (event) {
+//         event.preventDefault();
         
-        axios({
-            method: 'post',
-            url: '/api/posts',
-            data: {
-                title: title,
-                author: author,
-                content: content   
-            }
-        });
+//         axios({
+//             method: 'post',
+//             url: '/api/posts',
+//             data: {
+//                 title: title,
+//                 author: author,
+//                 content: content   
+//             }
+//         });
 
-        customAlert.fire({
-        title: <p>Post Criado com Sucesso!!</p>,
-        icon: 'success',
-        showConfirmButton: false,
-        timer: 5000,
-        })
+//         customAlert.fire({
+//         title: <p>Post Criado com Sucesso!!</p>,
+//         icon: 'success',
+//         showConfirmButton: false,
+//         timer: 5000,
+//         })
         
-        setTimeout(() => {
-            router.push('/');
-        }, 5000);
+//         setTimeout(() => {
+//             router.push('/');
+//         }, 5000);
 
-    }
+//     }
 
-    return (
-            <FormPageContainer>
-                <FormPost onSubmit={handleSubmit}>
-                    <Label htmlFor="title">Titulo</Label>
-                    <Input 
-                        type="text" 
-                        id="title" 
-                        name="title"
-                        value={title}
-                        onChange={(event) => setTitle(event.target.value)} />
-                    <Label htmlFor="author">Autor</Label>
-                    <Input 
-                        type="text" 
-                        name="author" 
-                        id="author"
-                        value={author}
-                        onChange={({target}) => setAuthor(target.value)} />
-                    <Label htmlFor="content">Post</Label>
-                    <TextArea 
-                        type="text" 
-                        name="content" 
-                        id="content"
-                        value={content}
-                        onChange={({target}) => setContent(target.value)}/>
-                    <SubmitInput type="submit" value="Postar"/>
-                </FormPost>
-            </FormPageContainer>
-    )
-}
+//     return (
+//             <FormPageContainer>
+//                 <FormPost onSubmit={handleSubmit}>
+//                     <Label htmlFor="title">Titulo</Label>
+//                     <Input 
+//                         type="text" 
+//                         id="title" 
+//                         name="title"
+//                         value={title}
+//                         onChange={(event) => setTitle(event.target.value)} />
+//                     <Label htmlFor="author">Autor</Label>
+//                     <Input 
+//                         type="text" 
+//                         name="author" 
+//                         id="author"
+//                         value={author}
+//                         onChange={({target}) => setAuthor(target.value)} />
+//                     <Label htmlFor="content">Post</Label>
+//                     <TextArea 
+//                         type="text" 
+//                         name="content" 
+//                         id="content"
+//                         value={content}
+//                         onChange={({target}) => setContent(target.value)}/>
+//                     <SubmitInput type="submit" value="Postar"/>
+//                 </FormPost>
+//             </FormPageContainer>
+//     )
+// }
 
-export default Create;
+// export default Create;
